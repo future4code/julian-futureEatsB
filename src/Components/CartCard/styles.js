1be6c4a73840252cf57b06ad090a5a3df9e5defa
Gislaine Costa Pereira <62241763+gislainecosta@users.noreply.github.com>
2020-06-16 20:18:32 -0300

    Lógica do carrinho

--- src/Components/CartCard/styles.js
+++ src/Components/CartCard/styles.js
@@ -5,7 +5,7 @@ export const CartCardContainer = styled.div`
   height: 20vh;
   border: grey 1px solid;
   width: 90%;
-  margin: 1vh auto;
+  margin: 1.25vh auto;
   border-radius: 8px;
   display: flex;
 `;
