33734b69cc01e07f00d160b36876b0bdb9e187d4
Walter <walteraandrade@gmail.com>
2020-06-16 01:27:04 -0300

    Restaurante

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
 import React from "react";
-import {Switch, Route, BrowserRouter } from "react-router-dom"
-import './App.css';
-import Login from './pages/login/Login'
-import Cart from './pages/cart/Carrinho'
-import Home from './pages/home/Home'
-import Perfil from './pages/perfil/Perfil'
-import Registro from './pages/registro/Registro'
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/login/Login";
+import Cart from "./pages/cart/Carrinho";
+import Home from "./pages/home/Home";
+import Perfil from "./pages/perfil/Perfil";
+import Registro from "./pages/registro/Registro";
+import Restaurante from "./pages/restaurante";
 
-
-const App = () => {  
-
+const App = () => {
   return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Login />
-          </Route>
-          <Route exact path="/home">
-            <Home />
-          </Route>
-          <Route exact path="/registro">
-            <Registro />
-          </Route>
-          <Route exact path="/perfil">
-            <Perfil />
-          </Route>
-          <Route exact path="/carrinho">
-            <Cart />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+        <Route exact path="/registro">
+          <Registro />
+        </Route>
+        <Route exact path="/perfil">
+          <Perfil />
+        </Route>
+        <Route exact path="/carrinho">
+          <Cart />
+        </Route>
+        <Route exact path="/restaurante">
+          <Restaurante />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
