eb6c5b14dbf93c5bfdb857aaa0003cd0dc8ce560
Gislaine Costa Pereira <62241763+gislainecosta@users.noreply.github.com>
2020-06-15 23:32:11 -0300

    Atualização do Router e início da Home

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Cart from './pages/cart/Carrinho'
 import Home from './pages/home/Home'
 import Perfil from './pages/perfil/Perfil'
 import Registro from './pages/registro/Registro'
+import Restaurante from './pages/restaurante/Restaurante'
+import Busca from './pages/busca/Busca'
 
 
 const App = () => {  
@@ -27,6 +29,12 @@ const App = () => {
           </Route>
           <Route exact path="/carrinho">
             <Cart />
+          </Route>
+          <Route exact path="/restaurante">
+            <Restaurante />
+          </Route>
+          <Route exact path="/busca">
+            <Busca />
           </Route>
         </Switch>
       </BrowserRouter>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,17 +2,64 @@ import React, { useState } from "react";
 import { useHistory } from "react-router";
 import styled from 'styled-components';
 import "./Home.css";
+import { createMuiTheme, ThemeProvider, withStyles} from '@material-ui/core/styles';
+import CardProduto from '../../Components/FeedCard/index';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import SearchIcon from '@material-ui/icons/Search';
 
+const IconeProcurar = withStyles({
+  root: {
+    width: '17.5px',
+    height: '17.5px',
+    color: '#b8b8b8',
+  },
+})(SearchIcon);
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+  textField: {
+    width: '328px',
+    height: '56px',
+    borderRadius: '2px',
+    border: 'solid 1px #b8b8b8',
+  },
+}));
+
+const MyTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#5cb646",
+      contrastText: "#000000",
+      light: "rgba(92, 182, 70, 0.5)",
+    }
+  }
+})
 
 
 const Home = (props) => {
-    
-  
+  const classes = useStyles();
+
+
   return (
-    <div className='telatoda'>
+    <ThemeProvider theme={MyTheme} className='telatoda'>
+      <div>
+        <p>Cabeçalho</p>
+      </div>
+        <div>
+          <OutlinedInput
+            id="outlined-adornment-weight"
+            placeholder="Restaurante"
+            startAdornment={<IconeProcurar color="disabled"/>}
+            className={clsx(classes.margin, classes.textField)}
+          />
+        </div>
+
      
-        <p>Oi, eu sou o Home</p>
-    </div>
+    </ThemeProvider>
   );
 };
 
