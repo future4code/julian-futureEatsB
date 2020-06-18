8295239d46ceb1fc84f137782347d1bf125aeb01
Gislaine Costa Pereira <62241763+gislainecosta@users.noreply.github.com>
2020-06-18 00:13:52 -0300

    Context para o carrinho

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useReducer} from "react";
 
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
@@ -14,51 +14,56 @@ import EditarEndereco from "./pages/editarEndereco/EditarEndereco";
 import Busca from "./pages/busca/Busca";
 import Endereco from "./pages/endereco";
 import PlaceholderCarrinho from "./pages/placeholderCarrinho";
-const urlBase =
-  "https://us-central1-missao-newton.cloudfunctions.net/futureEatsB";
+import { CartReducer, initialState } from "./functions/CardReducer";
+import CartContext from './functions/CartContext';
+const urlBase= "https://us-central1-missao-newton.cloudfunctions.net/futureEatsB";
 
 const App = () => {
+  const [state, dispatch] = useReducer(CartReducer, initialState);
+
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route urlBase={urlBase} exact path="/">
-          <Inicial />
-        </Route>
-        <Route urlBase={urlBase} exact path="/Login">
-          <Login />
-        </Route>
-        <Route urlBase={urlBase} exact path="/home">
-          <Home />
-        </Route>
-        <Route urlBase={urlBase} exact path="/registro">
-          <Registro />
-        </Route>
-        <Route urlBase={urlBase} exact path="/cadastro-endereco">
-          <Endereco />
-        </Route>
-        <Route urlBase={urlBase} exact path="/perfil">
-          <Perfil />
-        </Route>
-        <Route urlBase={urlBase} exact path="/perfil/cadastro">
-          <EditarCadastro />
-        </Route>
-        <Route urlBase={urlBase} exact path="/perfil/endereco">
-          <EditarEndereco />
-        </Route>
-        <Route urlBase={urlBase} exact path="/carrinho">
-          <Cart />
-        </Route>
-        <Route urlBase={urlBase} exact path="/restaurante/:pageID">
-          <Restaurante />
-        </Route>
-        <Route urlBase={urlBase} exact path="/busca">
-          <Busca />
-        </Route>
-        <Route urlBase={urlBase} exact path="/placeholder">
-          <PlaceholderCarrinho />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <CartContext.Provider value={{cart: state.cart, products: state.products, dispatch: dispatch }}>
+      <BrowserRouter>
+        <Switch>
+          <Route urlBase={urlBase} exact path="/">
+            <Inicial />
+          </Route>
+          <Route urlBase={urlBase} exact path="/Login">
+            <Login />
+          </Route>
+          <Route urlBase={urlBase} exact path="/home">
+            <Home />
+          </Route>
+          <Route urlBase={urlBase} exact path="/registro">
+            <Registro />
+          </Route>
+          <Route urlBase={urlBase} exact path="/cadastro-endereco">
+            <Endereco />
+          </Route>
+          <Route urlBase={urlBase} exact path="/perfil">
+            <Perfil />
+          </Route>
+          <Route urlBase={urlBase} exact path="/perfil/cadastro">
+            <EditarCadastro />
+          </Route>
+          <Route urlBase={urlBase} exact path="/perfil/endereco">
+            <EditarEndereco />
+          </Route>
+          <Route urlBase={urlBase} exact path="/carrinho">
+            <Cart />
+          </Route>
+          <Route urlBase={urlBase} exact path="/restaurante/:pageID">
+            <Restaurante />
+          </Route>
+          <Route urlBase={urlBase} exact path="/busca">
+            <Busca />
+          </Route>
+          <Route urlBase={urlBase} exact path="/placeholder">
+            <PlaceholderCarrinho />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </CartContext.Provider>
   );
 };
 
--- src/functions/CardReducer.js
+++ src/functions/CardReducer.js
@@ -1,20 +1,48 @@
 export const initialState = {
-  cart: [],
+  cart: [
+    {
+      "restaurant": {
+        "products": [{
+            "id": "2wvQI3i8Zf2fMvkEq4Vo",
+            "category": "Refeição",
+            "description": "A melhor pedida é japonesa!",
+            "price": 24.3,
+            "photoUrl": "https://static-images.ifood.com.br/image/upload/f_auto,t_high/pratos/51fed2ca-70a6-4069-a203-65536c856035/201808311212_sashi.png",
+            "name": "Sashimi Shiromi"
+          },
+          {
+            "id": "BmDOBMDVYDAomt7vQtuP",
+            "name": "Tempura udon",
+            "category": "Refeição",
+            "description": "A melhor pedida é japonesa!",
+            "price": 52.7,
+            "photoUrl": "https://static-images.ifood.com.br/image/upload/f_auto,t_high/pratos/51fed2ca-70a6-4069-a203-65536c856035/201802031948_20717381.jpg"
+          },
+        ],
+        "id": "10",
+        "name": "Tadashii",
+        "shipping": 13,
+        "description": "Restaurante sofisticado busca o equilíbrio entre ingredientes que realçam a experiência da culinária japonesa.",
+        "address": "Travessa Reginaldo Pereira, 130 - Ibitinga",
+        "logoUrl": "http://soter.ninja/futureFoods/logos/tadashii.png",
+        "deliveryTime": 50,
+        "category": "Asiática"
+      }
+    }
+  ],
   products: [
     {
       id: "3vcYYSOEf8dKeTPd7vHe",
       category: "Pastel",
       description: "Pastel autêntico, feito na hora!",
       price: 3,
-      photoUrl:
-        "https://static-images.ifood.com.br/image/upload/f_auto,t_high/pratos/65c38aa8-b094-413d-9a80-ddc256bfcc78/201907031408_66194519.jpg",
+      photoUrl: "https://static-images.ifood.com.br/image/upload/f_auto,t_high/pratos/65c38aa8-b094-413d-9a80-ddc256bfcc78/201907031408_66194519.jpg",
       name: "Pastel",
     },
     {
       id: "5omTFSOBYiTqeiDwhiBx",
       price: 1,
-      photoUrl:
-        "https://static-images.ifood.com.br/image/upload/f_auto,t_high/pratos/65c38aa8-b094-413d-9a80-ddc256bfcc78/201907031403_66194479.jpg",
+      photoUrl: "https://static-images.ifood.com.br/image/upload/f_auto,t_high/pratos/65c38aa8-b094-413d-9a80-ddc256bfcc78/201907031403_66194479.jpg",
       name: "Bibsfiha queijo",
       category: "Salgado",
       description: "Esfiha deliciosa, receita secreta do Habibs.",
