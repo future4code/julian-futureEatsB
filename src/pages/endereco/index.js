b1f6f6c1321d0e54e9c6c9b060214472c032834d
maxwelb-assis <max.assis@ymail.com>
2020-06-18 15:57:13 -0300

    api de cadastro e endereço

--- src/pages/endereco/index.js
+++ src/pages/endereco/index.js
@@ -1,37 +1,95 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
 import {Container, Input, Paragrafo, ContainerInput, Botao} from './styles'
 import Header from '../../Components/Header/index'
+import { useHistory } from "react-router";
 
 
 const Endereco = () =>{
+    const [logradouro, setLogradouro] = useState("")
+    const [numero, setNumero] = useState("")
+    const [complemento, setComplemento] = useState("")
+    const [bairro, setBairro] = useState("")
+    const [cidade, setCidade] = useState("")
+    const [estado, setEstado] = useState("")
     
+    const inputLogradouro = (event) => {
+        setLogradouro(event.target.value)
+    }
+
+    const inputNumero = (event) => {
+        setNumero(event.target.value)
+    }
+
+    const inputComplemento = (event) => {
+        setComplemento(event.target.value)
+    }
+
+    const inputBairro = (event) => {
+        setBairro(event.target.value)
+    }
+
+    const inputCidade = (event) => {
+        setCidade(event.target.value)
+    }
+
+    const inputEstado = (event) => {
+        setEstado(event.target.value)
+    }
+    
+    const history = useHistory() 
+
+    const goToHome = () =>{
+        history.push('/home')
+      }
+
+
+    const enviarInput = () => {
+           
+        const token = localStorage.getItem("token")
+
+        const body = {
+                street: logradouro,
+                number: numero,
+                neighbourhood: bairro,
+                city: cidade,
+                state: estado ,
+                complement: complemento
+            }
+
+        axios.put('https://us-central1-missao-newton.cloudfunctions.net/futureEatsB/address', body, {
+            headers: {
+                Auth: token
+        }}).then((response) => {
+            console.log(response)
+            localStorage.setItem("token", response.data.token)
+            goToHome()
+        }).catch(error => {
+            console.log(error.response)
+            alert('erro insira um endereço valido')
+        })
+
+    }
+
     return(
         <Container>
             <Header back/>
             <Paragrafo>Meu endereço</Paragrafo>
 
             <ContainerInput>
-            <Input label="logradouro" placeholder="Rua / Av." variant="outlined"/>
-            <Input label="numero" placeholder="numero" variant="outlined"/>
-            <Input label="complemento" placeholder="Apto. Bloco" variant="outlined"/>
-            <Input label="bairro" placeholder="Bairro" variant="outlined"/>
-            <Input label="cidade" placeholder="Cidade" variant="outlined"/>
-            <Input label="estado" placeholder="Estado" variant="outlined"/>
+            <Input onChange={inputLogradouro} label="logradouro" placeholder="Rua / Av." variant="outlined" required/>
+            <Input onChange={inputNumero} label="numero" placeholder="numero" variant="outlined" required type="number"/>
+            <Input onChange={inputComplemento} label="complemento" placeholder="Apto. Bloco" variant="outlined" required/>
+            <Input onChange={inputBairro} label="bairro" placeholder="Bairro" variant="outlined" required/>
+            <Input onChange={inputCidade} label="cidade" placeholder="Cidade" variant="outlined" required/>
+            <Input onChange={inputEstado} label="estado" placeholder="Estado" variant="outlined" required/>
 
-            <Botao>Salvar</Botao>
+            <Botao onClick={enviarInput}>Salvar</Botao>
 
             </ContainerInput>
-
-
-
-
-
         </Container>
 
-
     )
-
-
 
 }
 
--- src/pages/registro/Registro.js
+++ src/pages/registro/Registro.js
@@ -1,38 +1,113 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
 import {Container, Imagem, Paragrafo, ContainerInput, Input, Botao } from './styles'
 import Header from '../../Components/Header/index'
 import logo from '../../img/logo-invertido.png'
 import Password from '../../Components/InputPassword'
-
+import { useHistory } from "react-router";
 
 
 const Registro = () => {
+    const [nome, setNome] = useState("")
+    const [email, setEmail] = useState("")
+    const [cpf, setCpf] = useState("")
+    const [senha, setSenha ] = useState("")
+    const [confirmar, setConfirmar] = useState("")
+
+    const history = useHistory()
+
+    const inputNome = (event) => {
+        setNome(event.target.value)
+    }
+
+    const inputEmail = (event) => {
+        setEmail(event.target.value)
+    }
+
+    const inputCpf = (event) => {
+        setCpf(event.target.value)
+    }
+
+    const inputSenha = (event) => {
+        setSenha(event.target.value)
+    }
+
+    const inputConfirma = (event) => {
+        setConfirmar(event.target.value)
+    }
+
+    const enviarFormulario = () => {
+
+    const body = {
+    name: nome,
+	email: email,
+	cpf: cpf,
+	password: senha} 
+   
+    axios.post('https://us-central1-missao-newton.cloudfunctions.net/futureEatsB/signup', body
+    ).then((response) => {
+    console.log(response.data.token)
+    localStorage.setItem("token", response.data.token)
+    history.push("/cadastro-endereco")
+    }).catch(error => {
+    console.log(error.response)
+    alert("erro dados invalidos")
+    })
+
+    }
+
+
     return (
         <>
-        <Header back/>
+        <Header back/> 
         <Container>   
           <Imagem src={logo}/>
             <Paragrafo>Cadastrar</Paragrafo>
+            
             <ContainerInput>
-            <Input label="nome e sobrenome" variant="outlined"/>
-            <Input label="email" variant="outlined"/>
-            <Input label="cpf" variant="outlined"/>
-            <Password label="senha"/>
-            <Input label="confirmar" variant="outlined"/>
+            <Input 
+            onChange={inputNome} 
+            label="nome e sobrenome" 
+            variant="outlined" 
+            value={nome}
+            name='name'
+            />
+            <Input 
+            onChange={inputEmail} 
+            label="email" 
+            variant="outlined" 
+            value={email}
+            name='email'
+            />
+            <Input 
+            onChange={inputCpf} 
+            label="cpf" 
+            variant="outlined" 
+            value={cpf}
+            name='cpf'
+            />
+            <Password 
+            onChange={inputSenha} 
+            label="senha"   
+            value={senha}
+            name='password'
+            />
+            <Password 
+            onChange={inputConfirma} 
+            label="confirmar" 
+            variant="outlined" 
+            value={confirmar}
+            name='confirmar'
+            />
 
-            <Botao>Criar</Botao>
-
+            <Botao onClick={enviarFormulario}>Criar</Botao>
 
             </ContainerInput>
-            
-
-
-
+        
         </Container> </>
     )
 
 
-
-}
+} 
 
 export default Registro
